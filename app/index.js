297247ab15d1a29a33bcfe31351cad38c05bf2e6
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-20 18:41:02 -0700

    beginning routing

--- app/index.js
+++ app/index.js
@@ -1,16 +1,55 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: "Gabriel Matias",
+  username: "GMatias93",
+  image: "test"
+};
+
+var ProfilePic = React.createClass({
   render: function() {
     return (
-      <div> Hello World </div>
+      <img src={this.props.imageUrl} style={{height: 100, weidth: 100}} />
+    );
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={"https://www.github.com/" + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     );
   }
 });
 
 
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
